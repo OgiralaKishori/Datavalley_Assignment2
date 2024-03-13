73a84a6d399d18c7ef81c8b61d083feb817c4e5e
OgiralaKishori <121427560+OgiralaKishori@users.noreply.github.com>
2024-03-13 18:12:58 +0530

    Create Stringg.java

---
+++ Stringg.java
@@ -0,0 +1,17 @@
+
+class A {
+    public static void main(String[] args) {
+        String t="type here to search";
+        int i;
+        String s=" ";
+        
+        for (i=0;i<(t.length());i++)
+        {
+            if (i%2!=0 && t.charAt(i) !=' ')
+            {
+                s=s+t.charAt(i);
+            }
+        }
+        System.out.print(s);
+    }
+}
